2091411c7219d1f8f829fd3fab5639d1a945d09c
jazzqueenjessie <jazzqueenjessie@gmail.com>
2013-03-11 23:45:52 -0700

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 WeddingApp::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/story"
+
+  get "static_pages/travel"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
